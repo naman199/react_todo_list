f4cab3d3c4cd88a45ec565b49160dd94ff969da1
Abc <abc@ubuntu.ubuntu-domain>
2019-09-01 21:27:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import uuid from 'uuid';
+
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'Todo1',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Todo2',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Todo3',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })
+  });
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  //AddTodo
+  addTodo = (title) => {
+    const newTodo={
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
+
+  render(){
+    return(
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>Learning React To add React in my skills</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
